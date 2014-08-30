94b9a15c8b4beff4b4904588f6ae54044bf8f950
Chase Berry <chase@raspberrypi.(none)>
2014-08-30 03:08:27 +0000

    Added the doorLock and roomModule Working on the module to be the abstract class for each 'room controlling object'

---
+++ roomModule.py
@@ -0,0 +1,18 @@
+
+class roomModule:
+    
+    name
+    status
+
+    def __init__(self, name):
+        self.name = name
+        self.status = 0
+
+    def getStatus(self):
+        return {'statusCode': -1, 'status': 'No status function defined'} 
+
+    def receivePacket(self);
+        pass
+
+   
+   
